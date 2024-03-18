054f3d043d497205f7e1964d221621ffe9132e5b
P Maheshwaran <p.maheshwaran@connectandheal.com>
2024-03-18 10:32:54 +0530

    Update to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CssBaseline, ThemeProvider, useTheme } from '@mui/material';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 import MyRoutes from './routes/MyRoutes';
 import store from './store';
@@ -11,14 +11,14 @@ const App: React.FC = () => {
   const theme = useTheme();
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <ThemeProvider theme={createTheme(theme)}>
           <CssBaseline />
           <MyRoutes />
         </ThemeProvider>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
